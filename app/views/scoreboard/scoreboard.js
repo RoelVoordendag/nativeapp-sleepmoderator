ed0b131522844ac0846be35933be9d04225d5c89
RoelVoordendag <rvoordendag@gmail.com>
2018-01-15 23:18:45 +0100

    push

--- app/views/scoreboard/scoreboard.js
+++ app/views/scoreboard/scoreboard.js
@@ -2,11 +2,17 @@ var frameModule = require("ui/frame");
 var Observable = require("data/observable").Observable;
 var view = require("ui/core/view");
 var dialogs = require("ui/dialogs");
+var http = require("http");
+var observableModule = require("data/observable");
 
 var drawer;
 
 var pageData =  new Observable();
 
+var scoreGuus;
+var scoreMark;
+var scoreRoel;
+var latestSessionId;
 //function starts wen page is loaded
 function pageLoaded(args){
     //checking if you are logged in
@@ -30,10 +36,85 @@ function pageLoaded(args){
     //getting name from sidedrawer
     var name = view.getViewById(page, "name");
 
-    page.bindingContext = {
-        name:  global.currentUsername,
+    //calculating score for picture 
+    if(global.totalScore <=70 ){
+        page.getViewById("circle").backgroundImage = "~/images/badges/profile-brons.png";
+    }else if(global.totalScore >=70 && global.totalScore<=430){
+        page.getViewById("circle").backgroundImage = "~/images/badges/profile-silver.png";
+    }else if(global.totalScore >= 431){
+        page.getViewById("circle").backgroundImage = "~/images/badges/profile-gold.png";
     }
-   
+    
+    var scoreBoard = view.getViewById(page, "scoreBoard");
+
+        for(var i = 0; i <=3; i++){ 
+
+            console.log(i);
+
+            http.getJSON("http://markvonk.com/sleep/sessions.php?user=" + i).then(function (r){
+
+                latestSessionId = r[r.length-1].session_id;
+
+                console.log('session id' + latestSessionId);
+
+                console.log("i na het berekenen van session id" + i);
+        http.getJSON("http://markvonk.com/sleep/sessions.php?user=" + i + "&session=" + latestSessionId).then(function (r){
+
+
+                    console.log(i);
+
+                    if(i == 1 ){
+                        totalscore = r[0].score;
+                        scoreGuus = r[0].score;
+                        console.log('wat is deze score' + totalscore)
+                        if(totalscore <=70 ){
+                            page.getViewById("guus").backgroundImage = "~/images/badges/score-brons.png";
+                        }else if(totalscore >=70 && totalscore<=430){
+                            page.getViewById("guus").backgroundImage = "~/images/badges/score-silver.png";
+                        }else if(totalscore >= 431){
+                            page.getViewById("guus").backgroundImage = "~/images/badges/score-gold.png";
+                        }          
+                    }else if(i == 2){
+                        totalscore = r[0].score;
+                        scoreMark= r[0].score;
+                        console.log('wat is deze score' + totalscore)
+
+                        if(totalscore <=70 ){
+                            page.getViewById("mark").backgroundImage = "~/images/badges/score-brons.png";
+                        }else if(totalscore >=70 && totalscore<=430){
+                            page.getViewById("mark").backgroundImage = "~/images/badges/score-silver.png";
+                        }else if(totalscore >= 431){
+                            page.getViewById("mark").backgroundImage = "~/images/badges/score-gold.png";
+                        }          
+                    }else if(i == 3){
+                        totalscore = r[0].score;
+                        scoreRoel= r[0].score;
+                        console.log('wat is deze score' + totalscore)
+
+                        if(totalscore <=70 ){
+                            page.getViewById("roel").backgroundImage = "~/images/badges/score-brons.png";
+                        }else if(totalscore >=70 && totalscore<=430){
+                            page.getViewById("roel").backgroundImage = "~/images/badges/score-silver.png";
+                        }else if(totalscore >= 431){
+                            page.getViewById("roel").backgroundImage = "~/images/badges/score-gold.png";
+                        }          
+                    }
+                
+                })
+                })
+            }
+        
+            
+        
+    
+    var text = new observableModule.fromObject({
+        name: global.currentUsername,
+        guusScore: scoreGuus,
+        markScore: scoreMark,
+        roelScore: scoreRoel
+    });
+
+    page.bindingContext = text;   
 }
 exports.pageLoaded = pageLoaded;
 
